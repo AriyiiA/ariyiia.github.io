2b6e64cbef447e04829904a91922bd34bd46b320
AriyiiA <112403329+AriyiiA@users.noreply.github.com>
2023-01-17 09:22:21 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-const texteAnime = document.getElementById('typewrite');
+// const texteAnime = document.getElementById('typewrite');
 
-new Typewriter(texteAnime, {
-    deleteSpeed: 10 , loop: false
+// new Typewriter(texteAnime, {
+//     deleteSpeed: 10 , loop: false
     
-})
-.changeDelay(50)
-.typeString('<span id="title">Hello, my name is Sévan Gardes</span><br>')
-.pauseFor(500)
-.typeString('<br>I am 22 years old and I am passionate since my youngest age about new technologies')
-.deleteChars(12)
-.typeString('<span style="color:white;">computer science</span>.<br>')
-.pauseFor(500)
-.typeString('I am currently taking a training of web and mobile <span style="color:white;">web developer</span> during 1 year.<br>')
-.typeString('And I really apreciate it !')
-.pauseFor(1500)
-.start()
- console.log('cest ok');
+// })
+// .changeDelay(50)
+// .typeString('<span id="title">Hello, my name is Sévan Gardes</span><br>')
+// .pauseFor(500)
+// .typeString('<br>I am 22 years old and I am passionate since my youngest age about new technologies')
+// .deleteChars(12)
+// .typeString('<span style="color:white;">computer science</span>.<br>')
+// .pauseFor(500)
+// .typeString('I am currently taking a training of web and mobile <span style="color:white;">web developer</span> during 1 year.<br>')
+// .typeString('And I really apreciate it !')
+// .pauseFor(1500)
+// .start()
+// console.log('cest ok');
 (function() {
   
   var autoUpdate = false,
