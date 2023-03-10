1edb91947454e71601de4bc04ea59d76eae89ca7
Sévan gardes <sevan.gardes.geiqnum@gmail.com>
2023-03-10 13:23:54 +0100

    small changes

---
+++ .history/src/app_20230310111202.js
@@ -0,0 +1,148 @@
+// const texteAnime = document.getElementById('typewrite');
+
+// new Typewriter(texteAnime, {
+//     deleteSpeed: 10 , loop: false
+    
+// })
+// .changeDelay(50)
+// .typeString('<span id="title">Hello, my name is Sévan Gardes</span><br>')
+// .pauseFor(500)
+// .typeString('<br>I am 22 years old and I am passionate since my youngest age about new technologies')
+// .deleteChars(12)
+// .typeString('<span style="color:white;">computer science</span>.<br>')
+// .pauseFor(500)
+// .typeString('I am currently taking a training of web and mobile <span style="color:white;">web developer</span> during 1 year.<br>')
+// .typeString('And I really apreciate it !')
+// .pauseFor(1500)
+// .start()
+// console.log('cest ok');
+(function() {
+  
+  var autoUpdate = false,
+      timeTrans = 4000;
+  
+	var cdSlider = document.querySelector('.cd-slider'),
+		item = cdSlider.querySelectorAll("li"),
+		nav = cdSlider.querySelector("nav");
+
+	item[0].className = "current_slide";
+
+	for (var i = 0, len = item.length; i < len; i++) {
+		var color = item[i].getAttribute("data-color");
+		item[i].style.backgroundColor=color;
+	}
+
+	// Detect IE
+	// hide ripple effect on IE9
+	var ua = window.navigator.userAgent;
+		var msie = ua.indexOf("MSIE");
+		if ( msie > 0 ) {
+			var version = parseInt(ua.substring(msie+ 5, ua.indexOf(".", msie)));
+			if (version === 9) { cdSlider.className = "cd-slider ie9";}
+	}
+
+	if (item.length <= 1) {
+		nav.style.display = "none";
+	}
+
+	function prevSlide() {
+		var currentSlide = cdSlider.querySelector("li.current_slide"),
+			prevElement = currentSlide.previousElementSibling,
+			prevSlide = ( prevElement !== null) ? prevElement : item[item.length-1],
+			prevColor = prevSlide.getAttribute("data-color"),
+			el = document.createElement('span');
+
+		currentSlide.className = "";
+		prevSlide.className = "current_slide";
+
+		nav.children[0].appendChild(el);
+
+		var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
+		    ripple = nav.children[0].querySelector("span");
+
+		ripple.style.height = size + 'px';
+		ripple.style.width = size + 'px';
+		ripple.style.backgroundColor = prevColor;
+
+		ripple.addEventListener("webkitTransitionEnd", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+		ripple.addEventListener("transitionend", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+	}
+
+	function nextSlide() {
+		var currentSlide = cdSlider.querySelector("li.current_slide"),
+			nextElement = currentSlide.nextElementSibling,
+			nextSlide = ( nextElement !== null ) ? nextElement : item[0],
+			nextColor = nextSlide.getAttribute("data-color"),
+			el = document.createElement('span');
+
+		currentSlide.className = "";
+		nextSlide.className = "current_slide";
+
+		nav.children[1].appendChild(el);
+
+		var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
+			  ripple = nav.children[1].querySelector("span");
+
+		ripple.style.height = size + 'px';
+		ripple.style.width = size + 'px';
+		ripple.style.backgroundColor = nextColor;
+
+		ripple.addEventListener("webkitTransitionEnd", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+		ripple.addEventListener("transitionend", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+	}
+
+	updateNavColor();
+
+	function updateNavColor () {
+		var currentSlide = cdSlider.querySelector("li.current_slide");
+
+		var nextColor = ( currentSlide.nextElementSibling !== null ) ? currentSlide.nextElementSibling.getAttribute("data-color") : item[0].getAttribute("data-color");
+		var	prevColor = ( currentSlide.previousElementSibling !== null ) ? currentSlide.previousElementSibling.getAttribute("data-color") : item[item.length-1].getAttribute("data-color");
+
+		if (item.length > 2) {
+			nav.querySelector(".prev").style.backgroundColor = prevColor;
+			nav.querySelector(".next").style.backgroundColor = nextColor;
+		}
+	}
+
+	nav.querySelector(".next").addEventListener('click', function(event) {
+		event.preventDefault();
+		nextSlide();
+		updateNavColor();
+	});
+
+	nav.querySelector(".prev").addEventListener("click", function(event) {
+		event.preventDefault();
+		prevSlide();
+		updateNavColor();
+	});
+  
+  //autoUpdate
+  setInterval(function() {
+    if (autoUpdate) {
+      nextSlide();
+      updateNavColor();
+    };
+	},timeTrans);
+
+})();
